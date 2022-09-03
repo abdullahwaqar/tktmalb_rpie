d425e90630da326f4e1ed13ac97db0989372c5ad
aw <awr417h@pm.me>
2022-09-04 02:14:10 +0500

    asm!("nop");

--- README.md
+++ README.md
@@ -13,3 +13,6 @@ Disassembly of section .text._start:
     8010:       eaffffff        b       8014 <_start+0x4>
     8014:       eafffffe        b       8014 <_start+0x4>
 ```
+
+BCM2837 Datasheet: https://cs140e.sergio.bz/docs/BCM2837-ARM-Peripherals.pdf
+Concept [at](https://archive.fosdem.org/2017/schedule/event/programming_rpi3/attachments/slides/1475/export/events/attachments/programming_rpi3/slides/1475/bare_metal_rpi3.pdf)
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
+use core::{arch::asm, panic::PanicInfo, ptr::write_volatile};
 
-mod boot {
-    use core::arch::global_asm;
-
-    global_asm!(".section .text._start");
-}
+// ? 3F20_0008 1<<3->8 turn pin21 to an output
+// ? 3F20_001c gpio1_set 1<<21->8 turn pin21 on
+// ? 3F20_0028 gpio1_clear 1<<21->8 turn pin21 off
 
 #[no_mangle]
+#[link_section = ".text._start"]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    unsafe {
+        write_volatile(0x3F20_0008 as *mut u32, 1 << 3);
+
+        loop {
+            write_volatile(0x3F20_001c as *mut u32, 1 << 21);
+
+            for _ in 0..50000 {
+                asm!("nop");
+            }
+
+            write_volatile(0x3F20_0028 as *mut u32, 1 << 21);
+
+            for _ in 0..50000 {
+                asm!("nop");
+            }
+        }
+    }
 }
 
 /// * Handler for dealing with panic [hard_fault, soft_fault, bus_error,..]
